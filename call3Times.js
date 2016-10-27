fb5c7c8a94764389b01904be14dba95eead7edf8
Juan A Cortes <juan.cortes@gmail.com>
2016-10-27 17:23:44 -0400

    Use the call3Times function to print "Hello, world!" 3 times

---
+++ call3Times.js
@@ -0,0 +1,19 @@
+// Implement the following problem without using a while or a for loop unless you were explicitly asked to use loops.
+//
+// 3 times
+//
+// Given this function:
+//
+function call3Times(fun) {
+  fun();
+  fun();
+  fun();
+}
+// Use the call3Times function to print "Hello, world!" 3 times.
+//
+
+var hello = function() {
+  console.log('Hello, world!');
+};
+
+call3Times(hell);
