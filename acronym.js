6a20bc0787833f512357665fc02028e81e490c98
Juan A Cortes <juan.cortes@gmail.com>
2016-10-28 13:57:51 -0400

    Function acronym takes an array of words as argument and returns the acronym of the words. Using the reduce method to do this.

---
+++ acronym.js
@@ -0,0 +1,25 @@
+// Implement the following problem without using a while or a for loop unless you were explicitly asked to use loops.
+//
+// Acronym
+//
+// Write a function acronym that takes an array of words as argument and returns the acronym of the words. Use the reduce method to do this.
+//
+// > acronym(['very', 'important', 'person'])
+// 'VIP'
+// > acronym(['national', 'aeronautics', 'space', 'administration'])
+// 'NASA'
+
+var words = ['very', 'important', 'person'];
+
+function firstChar(word) {
+  return word[0].toUpperCase();
+}
+
+function acronym(words) {
+  var firstChars = words.map(firstChar);
+  return firstChars.reduce(function(a, b) {
+    return a + b;
+  });
+}
+
+console.log(acronym(words));
